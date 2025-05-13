84337d3b26959deb61521194a038f1442815edd9
Martin D'Uva <martin.duva@lightit.io>
2025-05-13 13:08:31 -0300

    refactor: merge context file inside loaders

--- lib/loaders.ts
+++ lib/loaders.ts
@@ -1,8 +1,23 @@
 import "cheerio";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import {
+  dirname,
+  fromFileUrl,
+  join,
+} from "https://deno.land/std@0.192.0/path/mod.ts";
 
-import { PROMTIOR_PDF_SRC, PROMTIOR_WEBSITE_SRC } from "./context.ts";
+const __dirname = dirname(fromFileUrl(import.meta.url));
+const promtiorSlideDeckPdfPath = join(
+  __dirname,
+  "../static/promtior-slides.pdf",
+);
+
+export const PROMTIOR_PDF_SRC = Deno.env.get("PROMTIOR_PDF_URL") ??
+  promtiorSlideDeckPdfPath;
+
+export const PROMTIOR_WEBSITE_SRC = Deno.env.get("PROMTIOR_WEBSITE_URL") ??
+  "https://www.promtior.ai/service";
 
 const pdfLoader = new PDFLoader(PROMTIOR_PDF_SRC);
 const webLoader = new CheerioWebBaseLoader(PROMTIOR_WEBSITE_SRC, {
